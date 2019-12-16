c6fe6912738dc42690152d60dccf41c970bf7ce3
Zsuzsanna Urbán <urbanzsuzsii@gmail.com>
2019-12-16 13:44:47 +0100

    file writer problems

--- data_manager.py
+++ data_manager.py
@@ -1,10 +1,28 @@
 import csv
 
-def get_all_data(filename, ):
+answers_titles = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
+
+def get_all_data(filename, titles):
     with open(filename) as file:
         file_reader = csv.DictReader(file, delimiter=',')
         datalist = []
         for row in file_reader:
-            datalist.append([row['id'], row['title'], row['user_story'], row['acceptance_criteria'],
-                                        row['business_value'], row['estimation'], row['status']])
+            data = []
+            for title in titles:
+                data.append(row[title])
+            datalist.append(data)
     return datalist
+
+def write_data(filename, updated_data, titles):
+    with open(filename, mode='w') as file:
+        file.writelines(titles.join(','))
+        file.write('\n')
+        for item in updated_data:
+            file.writelines(item.join(','))
+            file.write('\n')
+
+
+l = get_all_data('sample_data/answer.csv', answers_titles)
+l.append(['0', '1493398154', '4', '0', 'You need to use brackets: my_list = []', ''])
+print(l)
+write_data('sample_data/answer.csv', l, answers_titles)
